6ea5ee2ef9b9aa00101dc2f7535ba65db9bb79de
Miguel Quiroz <miguel881@live.cl>
2022-10-20 22:28:19 -0300

    Modificacion de clases

--- profesionales/models.py
+++ profesionales/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 # Create your models here.
 class Profesional(models.Model):
     imagen_profesional = models.ImageField(upload_to="profesional",null=True)
@@ -15,10 +15,14 @@ class Profesional(models.Model):
     especialidades = models.CharField(max_length=500,blank=True,null=True)
     servicios = models.CharField(max_length=300, blank=True,null=True)
     valor = models.IntegerField(max_length=15,blank=True,null=True)
+    # Info del registro
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha ingreso")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha actualizacion")
+    deleted_at =  models.DateTimeField(auto_now=False, verbose_name="Fecha eliminacion", blank=True, null=True)
 
-class Meta:
-    verbose_name = 'Profesional'
-    verbose_name_plural = 'Profesionales'
+    class Meta:
+        verbose_name = 'Profesional'
+        verbose_name_plural = 'Profesionales'
 
-def __str__(self):
-    return str(self.curp)
+    def __str__(self):
+        return self.curp
