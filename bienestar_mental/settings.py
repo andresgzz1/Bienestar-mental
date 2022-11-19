602c906221656b6fb31d18bde6db92be024b47ca
Cristian Quiroga <quirogacristian020@gmail.com>
2022-11-19 20:37:50 -0300

    ALLOWED_HOSTS

--- bienestar_mental/settings.py
+++ bienestar_mental/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1x5qyc^wg4%av)be-05&(cgnpyzfhhqls4g1@$))@h6k8%8&bt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['206.189.236.93']
+ALLOWED_HOSTS = []
 
 
 # Application definition
