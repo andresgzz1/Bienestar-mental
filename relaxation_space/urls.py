a5ea8925c6070154130c31786222b0b09080e0d1
Bastian Rojas <bastianrojaspoblete@gmail.com>
2022-12-03 01:36:18 -0300

    Agregar gif a modulo de administracion, falta validaciones

--- relaxation_space/urls.py
+++ relaxation_space/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('adminView_rp_delete/<idImage>/', views.adminView_rp_delete, name='adminView_rp_delete'),
     path('adminView_rp_update/<idImage>/', views.adminView_rp_update, name='adminView_rp_update'),
     # Administración de espacios de relajación (Gifs)
-    path('adminView_rp_gif/<idSpace>', views.adminView_rp_gif, name='adminView_rp_gif'),
+    path('adminView_rp_gif/<idSpace>/', views.adminView_rp_gif, name='adminView_rp_gif'),
+    path('rp_gif_add/<idSpace>/', views.rp_gif_add, name='rp_gif_add'),
     # Template principal de espacios de relajación
     path('relax_space_view/', views.relax_space_view, name='relax_space_view'),
     # Functions view
